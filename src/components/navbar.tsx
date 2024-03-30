babae76f213899712e92a60a5d876198c7346474
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2024-03-29 22:28:28 -0300

    add navbar and mobile navibar menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,5 +1,30 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import { MobileNav } from './mobile-nav';
+
 type Props = {};
 
-export const Navbar = (props: Props) => {
-  return <div>Navibar</div>;
+export const Navbar = () => {
+  return (
+    <nav className='flex-between fixed z-50 w-full bg-dark-2 px-6 py-4 lg:px-10'>
+      <Link href={'/'} className='flex items-center gap-1'>
+        <Image
+          src={'/icons/logo.svg'}
+          width={32}
+          height={32}
+          alt='Boom Logo'
+          className='max-sm:size-10'
+        />
+        <p className='text-[26px] font-extrabold text-white max-sm:hidden'>
+          Boom
+        </p>
+      </Link>
+      <div className='flex-between gap-5 '>
+        {/* Clerk - user */}
+
+        <MobileNav />
+      </div>
+    </nav>
+  );
 };
