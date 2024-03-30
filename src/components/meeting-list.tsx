48efe7c1d009851efd19311e2f95f6729620880f
luiz-gustavo0 <santosluizgustavo101@gmail.com>
2024-03-30 18:47:40 -0300

    add home-card component

---
+++ src/components/meeting-list.tsx
@@ -0,0 +1,48 @@
+'use client';
+
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+
+import { HomeCard } from './home-card';
+
+type MeetingState =
+  | 'isScheduleMeeting'
+  | 'isJoiningMeeting'
+  | 'isInstantMeeting'
+  | undefined;
+
+export const MeetingTypeList = () => {
+  const [meetingState, setMeetingState] = useState<MeetingState>();
+  const router = useRouter();
+  return (
+    <section className='grid grid-cols-1 gap-5 md:grid-cols-2 xl:grid-cols-4'>
+      <HomeCard
+        img='/icons/add-meeting.svg'
+        title='New Meeting'
+        description='Start an instant meeting'
+        handleClick={() => setMeetingState('isInstantMeeting')}
+      />
+      <HomeCard
+        img='/icons/join-meeting.svg'
+        title='Join Meeting'
+        description='via invitation link'
+        className='bg-blue-1'
+        handleClick={() => setMeetingState('isJoiningMeeting')}
+      />
+      <HomeCard
+        img='/icons/schedule.svg'
+        title='Schedule Meeting'
+        description='Plan your meeting'
+        className='bg-purple-1'
+        handleClick={() => setMeetingState('isScheduleMeeting')}
+      />
+      <HomeCard
+        img='/icons/recordings.svg'
+        title='View Recordings'
+        description='Meeting Recordings'
+        className='bg-yellow-1'
+        handleClick={() => router.push('/recordings')}
+      />
+    </section>
+  );
+};
